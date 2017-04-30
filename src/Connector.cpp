2330e920b676c885bbb6eef19f93f3e0c49c6f6d
jash <jash@schulterklopfer-productions.de>
2017-04-30 20:14:17 +0200

    outputs are now marked in connector

--- src/Connector.cpp
+++ src/Connector.cpp
@@ -13,7 +13,13 @@ void Connector::draw( ImVec2 offset, float scale ) {
     const ImVec2 sourcePosition = mSource->getDrawPosition(offset, scale);
     const ImVec2 targetPosition = mTarget->getDrawPosition(offset, scale);
     
+    const ImVec2 socketPosition =
+        (ImVec2)((ofPoint)sourcePosition +
+                 ((ofPoint)targetPosition - (ofPoint)sourcePosition).getNormalized()*20);
+    
+    
     ImGui::GetWindowDrawList()->AddLine(sourcePosition, targetPosition, 0xffffffff,5.f*scale);
+    ImGui::GetWindowDrawList()->AddCircleFilled(socketPosition,7.f*scale, 0xffffffff);
     
 }
 
